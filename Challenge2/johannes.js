694ee78f199dcf5aed7f042eba435b3e321650d5
Jade Burgess <jadeburgess1998@gmail.com>
2023-04-04 14:05:03 +0200

    commit - completed challenge 1 and 2

---
+++ Challenge2/johannes.js
@@ -0,0 +1,7 @@
+// johannes.js
+const firstname = "Johannes"
+const surname = "Potgieter"
+export const role = "Intern"
+
+const display = firstname + " " + surname + " (" + role + ")"
+document.querySelector('#johannes').innerText = display
